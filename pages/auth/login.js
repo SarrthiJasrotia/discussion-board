7965976795154c3596adbcab21878c83f7db30a5
Sarrthi <sarrthijasrotia05@gmail.com>
2023-01-04 12:48:27 -0600

    login page created

---
+++ pages/auth/login.js
@@ -0,0 +1,15 @@
+import {FcGoogle} from "react-icons/fc"
+
+export default function Login(){
+    return(
+        <div className="border-4 mt-32 p-10 text-white-700 rounded-lg">
+            <h2 className="text-2xl font-medium text-center">Sign in options</h2>
+            <div className="py-4">
+                
+                <button className="bg-white rounded-lg w-full flex align-middle p-4 gap-2 text-black font-medium"> < FcGoogle className="text-2xl"/> Sign in with Google</button>
+                    
+            </div>
+
+        </div>
+    )
+}
