50e7b48f672534b33d5f863a067baecbbced0ac6
Sarrthi <sarrthijasrotia05@gmail.com>
2023-01-04 11:50:22 -0600

    added login and home btn

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,17 @@
+import Link from "next/link"
+
 export default function Nav(){
     return (
-        <div>
-            <h1> Nav Bar</h1>
-        </div>
+       <nav className="flex justify-between item-center py-10">
+            <Link href="/">
+            <button className="text-lg font-medium">Discussion Board</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link href="/auth/login">
+                    <button className="py-2 px-4 text-sm bg-red-500 text-white rounded-lg font-medium">login</button>
+                </Link>
+            </ul>
+       </nav>
     )
 }
     
