43077194202f937e6052cd9f2ecaabf7ac789202
Sarrthi <sarrthijasrotia05@gmail.com>
2023-01-05 10:51:26 -0600

    added signin with popup logic

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,12 +1,26 @@
-import {FcGoogle} from "react-icons/fc"
+import {FcGoogle} from "react-icons/fc";
+import {signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {auth} from "../../utils/firebase";
+
 
 export default function Login(){
+
+
+    //goolgle sign code
+    const googleProvider = new GoogleAuthProvider();
+    const GoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider)
+        } catch (error) {
+            console.log("L")
+        }
+    }
     return(
         <div className="border-4 mt-32 p-10 text-white-700 rounded-lg">
             <h2 className="text-2xl font-medium text-center">Sign in options</h2>
             <div className="py-4">
                 
-                <button className="bg-white rounded-lg w-full flex align-middle p-4 gap-2 text-black font-medium"> < FcGoogle className="text-2xl"/> Sign in with Google</button>
+                <button onClick={GoogleLogin} className="bg-white rounded-lg w-full flex align-middle p-4 gap-2 text-black font-medium"> < FcGoogle className="text-2xl"/> Sign in with Google</button>
                     
             </div>
 
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase';
-import {getFireStore} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,12 +15,12 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: "G-NJR57VL29Z"
+ //
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 
 export const auth = getAuth();
-export const db = getFireStore(app);
+export const db = getFirestore(app);
