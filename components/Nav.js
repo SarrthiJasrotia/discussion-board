773bf1f037599b3a8c97526ae7e2653dc337cc90
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2023-01-07 14:09:10 -0600

    added logic to hide and show btn based on loggin

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,41 @@
 import Link from "next/link"
+import { auth } from '../utils/firebase';
+import { useAuthState } from "react-firebase-hooks/auth";
 
-export default function Nav(){
+
+export default function Nav() {
+
+    // so I can use state to use for things such as if user is logged in or not
+    const [user, loading] = useAuthState(auth)
+
+
+    // console.log("user",user)
     return (
-       <nav className="flex justify-between item-center py-10">
+        <nav className="flex justify-between item-center py-10">
             <Link href="/">
-            <button className="text-lg font-medium">Discussion Board</button>
+                <button className="text-lg font-medium">Discussion Board</button>
             </Link>
             <ul className="flex items-center gap-10">
-                <Link href="/auth/login">
-                    <button className="py-2 px-4 text-sm bg-red-500 text-white rounded-lg font-medium">login</button>
-                </Link>
+
+                {/* if no user show them the login button */}
+                {!user && (<Link href="/auth/login">
+                    <button className="py-2 px-4 text-sm bg-red-500 text-white rounded-lg font-medium">Login</button>
+                </Link>)}
+
+                {/*  if there is a user let them access the post and other features*/}
+                {user && (< div className="flex items-center gap-6">
+                    <Link href="/post">
+
+                        <button  className="font-medium bg-yellow-500 text-white py-2 px-4 rounded-md text-sm"> Post</button>
+                    </Link>
+
+                    <Link href="/dashboard">
+                        <img src={user.photoURL} className="rounded-2xl"/>
+                    </Link>
+
+                </div>)}
             </ul>
-       </nav>
+        </nav>
     )
 }
-    
+
